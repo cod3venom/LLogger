4183f22130aaf8c04c2d86a84fd01249adea2adf
LevanOstrovski <levan@skillsforge.co>
2023-02-22 23:09:43 +0100

    iniital commit

---
+++ LLoger/Interface/ILLoger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLoger.Interface
+{
+    public interface ILLoger
+    {
+        void LogInformation(dynamic log);
+        void LogWarning(dynamic log);
+        void LogError(dynamic log);
+    }
+}
---
+++ LLoger/Logger.cs
@@ -0,0 +1,32 @@
+﻿using LLoger.Interface;
+using System.Reflection;
+
+namespace LLoger
+{
+    public class Logger : ILLoger
+    {
+        private readonly log4net.ILog _logger;
+        public Logger()
+        {
+            this._logger = log4net.LogManager.GetLogger(
+                MethodBase.GetCurrentMethod().DeclaringType
+            );
+        }
+
+
+        public void LogInformation(dynamic log)
+        {
+            this._logger.Info(log);
+        }
+
+        public void LogError(dynamic log)
+        {
+            this._logger.Error(log);
+        }
+
+        public void LogWarning(dynamic log)
+        {
+            this._logger.Warn(log);
+        }
+    }
+}
